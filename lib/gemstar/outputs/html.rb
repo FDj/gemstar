51366cccf56c077be6e9b0d52a43516e8fa3be2b
Florian Dejako <fdejako@gmail.com>
2025-10-05 15:11:02 +0200

    Show from, to, and created at at top

--- lib/gemstar/outputs/html.rb
+++ lib/gemstar/outputs/html.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative "basic"
 require "pathname"
 require "kramdown"
@@ -8,24 +10,24 @@ module Gemstar
       def render_diff(diff_command)
         body = diff_command.updates.map do |gem_name, info|
           icon = info[:homepage_url]&.include?("github.com") ? "🐙" : "💎"
-          tooltip = info[:description] ? "title=\"#{info[:description].gsub('"', '&quot;')}\"" : ""
+          tooltip = info[:description] ? "title=\"#{info[:description].gsub('"', "&quot;")}\"" : ""
           link = "<a href=\"#{info[:homepage_url]}\" #{tooltip} target=\"_blank\">#{gem_name}</a>"
           html = if info[:sections]
-                   info[:sections].map do |version, lines|
-                     <<~HTML
-                       #{Kramdown::Document.new(lines.join).to_html}
-                     HTML
-                   end.join("\n")
-                 elsif info[:release_urls]
-                   "" # changelog not found, but we have release links — skip message
-                 else
-                   "<p><strong>#{gem_name}:</strong> No changelog entries found</p>"
-                 end
+            info[:sections].map do |_version, lines|
+              <<~HTML
+                #{Kramdown::Document.new(lines.join).to_html}
+              HTML
+            end.join("\n")
+          elsif info[:release_urls]
+            "" # the changelog wasn't found, but we have release links — skip the message
+          else
+            "<p><strong>#{gem_name}:</strong> No changelog entries found</p>"
+          end
 
           <<~HTML
             <section>
-              <h2>#{icon} #{link}: #{info[:old] || 'new'} → #{info[:new]}</h2>
-              #{info[:release_page] ? "<p><a href='#{info[:release_page]}' target='_blank'>View all GitHub release notes</a></p>" : ""}
+              <h2>#{icon} #{link}: #{info[:old] || "new"} → #{info[:new]}</h2>
+              #{"<p><a href='#{info[:release_page]}' target='_blank'>View all GitHub release notes</a></p>" if info[:release_page]}
               #{html}
             </section>
           HTML
@@ -50,12 +52,11 @@ module Gemstar
           </head>
           <body>
             <h1>#{project_name}: Gem Updates</h1>
+            <p>Showing changes from #{diff_command.from} to #{diff_command.to || "now"}, generated on #{Time.now.strftime("%Y-%m-%d %H:%M:%S %z")}</p>
             #{body}
-            <p>Generated on #{Time.now.strftime("%Y-%m-%d %H:%M:%S %z")}</p>
           </body>
           </html>
         HTML
-
       end
     end
   end
